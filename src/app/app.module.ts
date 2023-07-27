664131f8fe172276db6fe86f14cdbe1472815120
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-26 21:37:06 -0600

    Set table design styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { IdentificationFormComponent } from './components/identification-form/identification-form.component';
 import { IdentificationTableComponent } from './components/identification-table/identification-table.component';
+import { EditIconComponent } from './components/identification-table/edit-icon/edit-icon.component';
+import { DeleteIconComponent } from './components/identification-table/delete-icon/delete-icon.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { IdentificationTableComponent } from './components/identification-table/
     HomeComponent,
     IdentificationFormComponent,
     IdentificationTableComponent,
+    EditIconComponent,
+    DeleteIconComponent,
   ],
   imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
